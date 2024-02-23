92e3617719dc9bc7efdc34faecbbd7ed6a315d55
Marian Wegner <marian.wegner@biontech.de>
2024-02-23 15:51:33 +0100

    Switch to using tfe.outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 ##################################################################################
 # OUTPUT
 ##################################################################################
+
+output "vpc_id" {
+  value       = module.main.vpc_id
+  description = "VPC ID"
+
+}
+
+output "publics_subnets" {
+  value       = module.main.public_subnets
+  description = "PUBLIC SUBNETS List"
+
+}
